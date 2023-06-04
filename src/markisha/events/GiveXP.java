1e9f94aa4c12ea61ca3a0084cbd35a7911d65adc
marko <marko.tanasic01@gmail.com>
2023-06-04 18:53:10 +0200

    fixed infinite xp with silk touch glitch

--- src/markisha/events/GiveXP.java
+++ src/markisha/events/GiveXP.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GiveXP implements Listener {
 
@@ -22,8 +25,37 @@ public class GiveXP implements Listener {
 				Material.COPPER_ORE
 			));
 	
+	private List<Material> pickaxes = new ArrayList<Material>(List.of(
+				Material.DIAMOND_PICKAXE,
+				Material.GOLDEN_PICKAXE,
+				Material.IRON_PICKAXE,
+				Material.NETHERITE_PICKAXE,
+				Material.STONE_PICKAXE,
+				Material.WOODEN_PICKAXE
+			));
+	
+	private boolean holdingPickaxe(BlockBreakEvent e) {
+		Player p = e.getPlayer();
+		ItemStack i = p.getInventory().getItemInMainHand();
+		
+		for (Material m : pickaxes) {
+			if (i.getType().equals(m)) {
+				if (i.getEnchantments().containsKey(Enchantment.SILK_TOUCH)) {
+					return false;
+				}
+				
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	@EventHandler
 	public void onOreBreak(BlockBreakEvent e) {
+		if (!holdingPickaxe(e)) 
+			return;
+		
 		Block b = e.getBlock();
 		Material m = b.getType();
 		World w = b.getWorld();
